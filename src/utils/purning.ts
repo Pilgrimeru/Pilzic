69e22c7411c2a664a5f7581a3d2b3d8993ef99a1
Pilgrimeru <84448802+Pilgrimeru@users.noreply.github.com>
2023-09-30 22:45:43 +0200

    purning 15sec to 25

--- src/utils/purning.ts
+++ src/utils/purning.ts
@@ -4,10 +4,9 @@ import { config } from "../config";
 export async function purning(msg: Message | InteractionResponse, long?: boolean) {
 
   if (!config.PRUNING) return;
-  let time = long ? 120 : 15;
+  let time = long ? 120 : 25;
 
   setTimeout(() => {
     msg.delete().catch(() => null);
   }, time * 1000);
-
 }
