aa06cc8943d8a6995a20a88daa9f78225635d818
Pilgrimeru <84448802+Pilgrimeru@users.noreply.github.com>
2023-10-01 13:41:24 +0200

    clean code

--- src/config.ts
+++ src/config.ts
@@ -1,17 +1,29 @@
 import { config as dotenvConfig } from 'dotenv';
-import { Config } from "./types/Config";
+import { Config } from './types/Config';
 
-let config: Config;
-dotenvConfig({ path: 'config.env' })
+dotenvConfig({ path: 'config.env' });
 
-config = {
+function parseEnvInt(value: string | undefined, defaultValue: number): number {
+  const parsedValue = parseInt(value || '', 10);
+  return isNaN(parsedValue) ? defaultValue : parsedValue;
+}
+
+function parseEnvAudioQuality(value: string | undefined, defaultValue: 0 | 1 | 2): 0 | 1 | 2 {
+  const parsedValue = parseEnvInt(value, 0);
+  if ([0, 1, 2].includes(parsedValue)) {
+    return parsedValue as 0 | 1 | 2;
+  }
+  return defaultValue;
+}
+
+const config: Config = {
   TOKEN: process.env.TOKEN || "",
   PREFIX: process.env.PREFIX || "!",
-  MAX_PLAYLIST_SIZE: (parseInt(process.env.MAX_PLAYLIST_SIZE!) > 1) ? parseInt(process.env.MAX_PLAYLIST_SIZE!) : 1 || 10,
-  PRUNING: process.env.PRUNING === "true" ? true : false,
-  STAY_TIME: parseInt(process.env.STAY_TIME!) || 10,
-  AUDIO_QUALITY: parseInt(process.env.AUDIO_QUALITY!) >= 0 && parseInt(process.env.AUDIO_QUALITY!) <= 2 ? parseInt(process.env.AUDIO_QUALITY!) as 0 | 1 | 2 : 0,
-  DEFAULT_VOLUME: parseInt(process.env.DEFAULT_VOLUME!) || 100,
+  MAX_PLAYLIST_SIZE: parseEnvInt(process.env.MAX_PLAYLIST_SIZE, 10),
+  PRUNING: Boolean(process.env.PRUNING),
+  STAY_TIME: parseEnvInt(process.env.STAY_TIME, 30),
+  AUDIO_QUALITY: parseEnvAudioQuality(process.env.AUDIO_QUALITY, 0),
+  DEFAULT_VOLUME: parseEnvInt(process.env.DEFAULT_VOLUME, 100),
   LOCALE: process.env.LOCALE || "en"
 };
 
