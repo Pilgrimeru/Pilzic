64d123e79739f5f95f211811493b05c72e5567b9
Pilgrimeru <84448802+Pilgrimeru@users.noreply.github.com>
2023-10-06 20:00:15 +0200

    invite response

--- src/commands/invite.ts
+++ src/commands/invite.ts
@@ -15,6 +15,7 @@ export default class InviteCommand extends Command {
       await commandTrigger.member!.send(
         `https://discord.com/oauth2/authorize?client_id=${commandTrigger.guild.client.user!.id}&permissions=274897914880&scope=bot`
       );
+      commandTrigger.reply(i18n.__mf("invite.result"));
     } catch (message) {
       return console.error(message);
     }
