7d16bf73322e455df08ab2dd79ef82e501e5ae57
Pilgrimeru <pilgrim@eru.com>
2024-10-31 20:49:52 +0100

    fix: missing autoDelete

--- src/commands/invite.ts
+++ src/commands/invite.ts
@@ -1,5 +1,6 @@
 import { CommandTrigger } from '@core/helpers/CommandTrigger';
 import { Command } from '@custom-types/Command';
+import { autoDelete } from '@utils/autoDelete';
 import { i18n } from 'i18n.config';
 
 export default class InviteCommand extends Command {
@@ -16,7 +17,7 @@ export default class InviteCommand extends Command {
       await commandTrigger.member!.send(
         `https://discord.com/oauth2/authorize?client_id=${commandTrigger.guild.client.user!.id}&permissions=274897914880&scope=bot`
       );
-      commandTrigger.reply(i18n.__mf("invite.result"));
+      commandTrigger.reply(i18n.__mf("invite.result")).then(autoDelete);
     } catch (message) {
       return console.error(message);
     }
